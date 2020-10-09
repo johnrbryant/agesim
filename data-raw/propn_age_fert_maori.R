3edd1886ead9611a6e27da91f7120b48ca59c41f
John Bryant <john.r.bryant@gmail.com>
2020-10-09 14:23:28 +1300

    updated maori fert data

--- data-raw/propn_age_fert_maori.R
+++ data-raw/propn_age_fert_maori.R
@@ -8,7 +8,6 @@ propn_age_fert_maori <- read_csv("data-raw/DFM168104_20200824_044821_8.csv",
                                  col_names = c("age", "rate"),
                                  n_max = 8) %>%
     mutate(age = cleanAgeGroup(age)) %>%
-    filter(age != "10-14") %>%
     mutate(propn = rate / sum(rate)) %>%
     dtabs(propn ~ age)
     
